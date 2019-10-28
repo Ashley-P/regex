120261730d92dc59320a03c67b5f4953eaec5621
Ashley-P <ashleyp1244@hotmail.co.uk>
2019-10-28 12:12:12 +0000

    regex.c : Added some preprocessor stuff

--- src/regex.c
+++ src/regex.c
@@ -2,6 +2,8 @@
 
 void regex(char *pattern, char *string) {
     // Echoing back for no real reason
+#ifndef REGEX_DEBUG
     printf("Pattern : \"%s\"\n", pattern);
     printf("String  : \"%s\"\n", string);
+#endif
 }
