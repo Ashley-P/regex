d72516be8ea731edf8c6f61f762e29f76d2d702a
Ashley-P <ashleyp1244@hotmail.co.uk>
2019-10-31 23:35:06 +0000

    regex.c : Setup a dummy function for fragmenting the pattern

--- src/regex.c
+++ src/regex.c
@@ -1,16 +1,26 @@
+/**
+ * Features currently implemented:
+ * None
+ */
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
 
 
-// Structs
+/***** Structs *****/
+/**
+ * Short_hands aren't included in StateType because they should get converted to something else
+ * e.g \w get's changed to a character set matching [a-zA-Z0-9_]
+ */
 typedef enum {
     LITERAL_CH = 1,
     META_CH    = 2,
-    RANGE      = 4, // For character sets e.g [A-Z]
+    RANGE      = 4, // For character sets (e.g [A-Z])
+    FINAL      = 8, // Special State that ends the regex engine (Might not be needed actually)
 } StateType;
-
-
 
 typedef struct State_ {
     StateType type;
@@ -21,9 +31,23 @@ typedef struct State_ {
 } State;
 
 
+typedef enum {
+    LITERAL_STR = 1, // Strings that can easily be converted to states (e.g abc)
+    META_CH     = 2, // e.g . + ?
+    RANGE       = 4, // For character sets (e.g [A-Z])
+    SHORT_HAND  = 8, // e.g \w \b
+} FragType;
 
-// Function Prototypes
+typedef struct Frag_ {
+    FragType type;
+    char *str;
+} Frag;
+
+
+
+/***** Function Prototypes *****/
 State *pattern_to_fsm(char *pattern);
+Frag *pattern_fragmenter(char *pattern);
 
 
 
@@ -37,15 +61,26 @@ void regex(char *pattern, char *string) {
 #endif
 
     State *start_state = pattern_to_fsm(pattern);
+
+#ifndef REGEX_DEBUG
     if (start_state == NULL) {
         printf("Something went wrong\n");
         exit(0);
+    } else {
+        printf("%p\n", start_state);
     }
+#endif
 
 }
 
 // Converts the pattern supplied to a Finite State Machine
 State *pattern_to_fsm(char *pattern) {
+    // First we break the pattern down into pattern fragments
+    
 
-    return NULL;
+
+    State *start_state = malloc(sizeof(State));
+    State *current_state = start_state;
+
+    return start_state;
 }
