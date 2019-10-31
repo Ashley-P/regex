9b78bc77900466564fe83c50d21e7d6ad8f6644d
Ashley-P <ashleyp1244@hotmail.co.uk>
2019-10-31 21:14:03 +0000

    regex.c : Defining some structs

--- src/regex.c
+++ src/regex.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+typedef enum {
+    LITERAL_CH = 1,
+    META_CH    = 2,
+    RANGE      = 4, // For character sets e.g [A-Z]
+} StateType;
+
+typedef struct State_ {
+    StateType type;
+    void *a;
+
+    struct State_ *next1;
+    struct State_ *next2;
+} State;
+
 void regex(char *pattern, char *string) {
     // Echoing back for no real reason
 #ifndef REGEX_DEBUG
