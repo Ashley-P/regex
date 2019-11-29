96c1eb4fd3af77a8e31929d9aa4cbc98fee98d63
Ashley-P <ashleyp1244@hotmail.co.uk>
2019-11-29 23:31:27 +0000

    regex.c : Very basic implementation after code refactor Only literal character matching

--- src/regex.c
+++ src/regex.c
@@ -33,6 +33,7 @@ typedef enum {
     // Special States
     S_START = 1,
     S_FINAL,
+    S_NODE,
 
     // Normal States
     S_LITERAL_CH,
@@ -55,20 +56,51 @@ typedef struct State_ {
 } State;
 
 
+typedef struct StateList_ {
+    struct State_ **l[MAX_BUFFER_SIZE];
+    int n;
+} StateList;
+
 // Fragments of the state machine
 typedef struct Fragment_ {
     struct State_ *start;
-
-    State **l[MAX_BUFFER_SIZE];
-    int n;
+    struct StateList_ *list;
 } Fragment;
+
+
+typedef enum {
+    T_LITERAL_CH  = 0x00001,
+    T_META_CH     = 0x00010, // For non state altering meta characters e.g '.'
+    T_FINAL       = 0x00100,
+
+    T_GREEDY_PLUS = 0x01000, // '+'
+    T_LAZY_PLUS   = 0x10000,   // '+?'
+
+    T_STATE_ALTERING = T_GREEDY_PLUS | T_LAZY_PLUS,
+#if 0
+    // Parentheses
+    T_OPEN_P,
+    T_CLOSE_P,
+
+    // Square brackets
+    T_OPEN_SB,
+    T_CLOSE_SB,
+#endif
+} TokenType;
+
+typedef struct Token_ {
+    TokenType type;
+    char ch;
+} Token;
 
 
 
 
 
 /***** Function Prototypes *****/
-Fragment pattern_to_fsm(char **pattern);
+Token *tokenize_pattern(char *pattern);
+State *parse_tokens(Token *tokens);
+Fragment *link_fragments(Fragment *fp, Token *tokens);
 
 char *perform_regex(State *start, char *string);
 
@@ -76,98 +108,169 @@ char *perform_regex(State *start, char *string);
 State *create_state(StateType type, StateData data, State * const next1, State * const next2);
 static inline void next_state(State **l, int *p, State **s, char **string);
 
+Fragment create_fragment(State *s, StateList *l);
+void point_state_list(StateList *l, State *a);
+StateList *create_state_list(State **first);
+StateList *append_lists(StateList *a, StateList *b);
+
+char *state_type_to_string(StateType type);
+char *token_type_to_string(TokenType type);
+
+
+
 
 char *regex(char *pattern, char *string) {
     // Echoing back for no real reason
-#ifndef REGEX_DEBUG
     printf("Pattern : \"%s\"\n", pattern);
     printf("String  : \"%s\"\n", string);
-#endif
 
-    Fragment complete = pattern_to_fsm(&pattern);
-    //char *return_str = perform_regex(start_state, string);
-    char *return_str = "";
+    // We'd do some checking here maybe
+    Token *tokens = tokenize_pattern(pattern);
+    // We'd do some checking here maybe
+    State *start = parse_tokens(tokens);
+    char *return_str = perform_regex(start, string);
 
-#ifndef REGEX_DEBUG
     if (*return_str == '\0')
         printf("Regex Failed\n");
     else
         printf("Returned string : %s\n", return_str);
-#endif
     return return_str;
 }
 
 
-/**
- * Converts the pattern supplied to a Finite State Machine Fragment
- * Returns a single fragment
- */
-Fragment pattern_to_fsm(char **pattern) {
-    Fragment rtn;
-    return rtn;
+Token *tokenize_pattern(char *pattern) {
+    printf("\n----- Tokenizing pattern -----\n");
+    Token *tokens = malloc(sizeof(Token) * MAX_BUFFER_SIZE);
+    Token *tp = tokens;
+    Token t;
+
+    while (*pattern != '\0') {
+        switch (*pattern) {
+            case '.':
+                t.type = T_META_CH;
+                t.ch = *pattern++;
+                *tp++ = t;
+                break;
+
+            // Literal Characters
+            default:
+                t.type = T_LITERAL_CH;
+                t.ch = *pattern++;
+                *tp++ = t;
+                break;
+        }
+        printf("TokenType = %s : ch = %c\n", token_type_to_string((tp - 1)->type), (tp - 1)->ch);
+    }
+
+    t.type = T_FINAL;
+    t.ch = '\0';
+    *tp++ = t;
+
+    printf("Tokenizing complete\n");
+    return tokens;
+}
+
+
+State *parse_tokens(Token *tokens) {
+    printf("\n----- Parsing tokens -----\n");
+
+    Fragment fragments[MAX_BUFFER_SIZE];
+    Fragment *fp = fragments;
+    Fragment a, b;
+
+    StateData data;
+    data.ch = '\0';
+    State *start = create_state(S_START, data, NULL, NULL);
+    State *s;
+
+    for (int i = 0; i < MAX_BUFFER_SIZE; i++) {
+        fragments[i] = create_fragment(NULL, NULL);
+    }
+
+    *fp++ = create_fragment(start, create_state_list(&start->next1));
+
+    while (tokens->type != T_FINAL) {
+        switch (tokens->type) { 
+            case T_LITERAL_CH:
+                data.ch = tokens->ch;
+                s = create_state(S_LITERAL_CH, data, NULL, NULL);
+                *fp++ = create_fragment(s, create_state_list(&s->next1));
+                printf("State %p, Type = %s, ch = %c\n",
+                        (void *) s, state_type_to_string(s->type), s->data.ch);
+                // Concatanation of the top 2 fragments on the stack happen if the next token isn't
+                // one that alters the flow of the FSM
+                fp = link_fragments(fp, tokens);
+                tokens++;
+                break;
+            default:
+                printf("nani\n");
+                tokens++;
+                break;
+        }
+    }
+
+    // If we aren't left with one fragment at this point something has gone wrong
+    if (fragments != fp-1) {
+        printf("Uh oh\n");
+        exit(0);
+    }
+
+    // Adding the final state
+    data.ch = '\0';
+    s = create_state(S_FINAL, data, NULL, NULL);
+    point_state_list((*--fp).list, s);
+
+    printf("Parsing complete\n");
+
+    return start;
+}
+
+Fragment *link_fragments(Fragment *fp, Token *tokens) {
+    // Peeking the next token
+    if (!((tokens + 1)->type & T_STATE_ALTERING)) {
+        Fragment b = *--fp;
+        Fragment a = *--fp;
+        point_state_list(a.list, b.start);
+        *fp++ = create_fragment(a.start, b.list);
+    }
+
+    return fp;
 }
 
 // Naviagtes the FSM and (should) returns the matched sub-string
 char *perform_regex(State *start, char *string) {
-    // We would do some storage of states that we can backtrack to
-    State *s = start->next1;
-    State *backtrack_list[MAX_BUFFER_SIZE];
-    int btp = 0;
-    if (start->next2) {
-        backtrack_list[btp++] = start->next2;
-    }
+    printf("\n----- Navigating Finite State Machine -----\n");
 
-    printf("\n");
+    State *s = start->next1; // This is the state we are checking
+    char *rtn_str = malloc(sizeof(char) * MAX_BUFFER_SIZE);
+    char *sp = rtn_str;
 
-    while(1) {
-        switch(s->type) {
+    while (1) {
+        printf("State %p, ", (void *) s);
 
-            case S_META_CH:
-                switch (s->data.meta) {
-                    case M_ANY_CH:
-                        // Any ch matches anything that isn't '\0'
-                        if (*string != '\0') {
-                            printf("State %p\tMeta character '.' (M_ANY_CH) matched with \"%c\"\n",
-                                    (void *) s, *string);
-
-                            next_state(backtrack_list, &btp, &s, &string);
-
-                        } else {
-                            printf("State %p\tMeta character '.' (M_ANY_CH) didn't match with \"%c\"\n",
-                                    (void *) s, *string);
-                            return ""; // Backtracking goes here
-                        }
-                        break;
-                    default:
-                        printf("Error in perform regex: Unknown MetaChType\n");
-                        break;
-                }
-                break;
-
+        switch (s->type) {
             case S_LITERAL_CH:
                 if (s->data.ch == *string) {
-                    printf("State %p\tLiteral character \"%c\" matched with \"%c\"\n",
-                            (void *) s, s->data.ch, *string);
-
-                    next_state(backtrack_list, &btp, &s, &string);
-
+                    printf("Literal character \"%c\" matched character \"%c\" in string\n",
+                            s->data.ch, *string);
+                    *sp++ = *string++;
+                    s = s->next1;
                 } else {
-                    // Backtrack
-                    printf("State %p\tLiteral character \"%c\" didn't match with \"%c\"\n",
-                            (void *) s, s->data.ch, *string);
+                    // Backtracking would happen here
+                    printf("Literal character \"%c\" did not match character \"%c\" in string\n",
+                            s->data.ch, *string);
                     return "";
                 }
                 break;
-
             case S_FINAL:
-                return "Completed Matching successfully";
-
+                printf("Match completed!\n");
+                *sp++ = '\0';
+                return rtn_str;
             default:
-                printf("Error in perform regex: default hit on the switch statement\n");
+                printf("Shouldn't hit this\n");
                 break;
         }
     }
-    return "";
 }
 
 
@@ -190,4 +293,63 @@ static inline void next_state(State **l, int *p, State **s, char **string) {
 
     *s = (*s)->next1;
     (*string)++;
+}
+
+Fragment create_fragment(State *s, StateList *l) {
+    Fragment rtn;
+    rtn.start = s;
+    rtn.list = l;
+    return rtn;
+}
+
+// The pointers from the first fragment are pointed the the start of the next one
+void point_state_list(StateList *l, State *a) {
+    for (int i = 0; i < l->n; i++) {
+        *(l->l)[i] = a;
+    }
+}
+
+StateList *create_state_list(State **first) {
+    StateList *l = malloc(sizeof(StateList));
+    l->n = 0;
+    l->l[l->n++] = first;
+
+    return l;
+}
+
+StateList *append_lists(StateList *a, StateList *b) {
+    StateList *rtn = malloc(sizeof(StateList));
+
+    for (int i = 0; i < a->n; i++)
+        rtn->l[i] = a->l[i];
+
+    for (int i = 0; i < b->n; i++)
+        rtn->l[i + rtn->n] = a->l[i];
+
+    free(a);
+    free(b);
+    return rtn;
+}
+
+char *state_type_to_string(StateType type) {
+    switch (type) {
+        case S_START: return "S_START";
+        case S_FINAL: return "S_FINAL";
+        case S_NODE: return "S_NODE";
+        case S_LITERAL_CH: return "S_LITERAL_CH";
+        case S_META_CH: return "S_META_CH";
+        case S_RANGE: return "S_RANGE";
+        default: return "Unhandled case in state_type_to_string";
+    }
+}
+
+char *token_type_to_string(TokenType type) {
+    switch (type) {
+        case T_LITERAL_CH: return "T_LITERAL_CH";
+        case T_META_CH: return "T_META_CH";
+        case T_FINAL: return "T_FINAL";
+        case T_GREEDY_PLUS: return "T_GREEDY_PLUS";
+        case T_LAZY_PLUS: return "T_LAZY_PLUS";
+        default: return "Unhandled case in token_type_to_string";
+    }
 }
